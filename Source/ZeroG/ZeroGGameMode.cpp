01f4982f9f5bdb383b8d7806106c1e16bea23ce8
karl <github@bigfun.co.za>
2024-08-06 23:42:27 +0200

    ZeroG

---
+++ Source/ZeroG/ZeroGGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ZeroGGameMode.h"
+#include "ZeroGCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AZeroGGameMode::AZeroGGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPerson/Blueprints/BP_FirstPersonCharacter"));
+	//static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/_ZeroG/Characters/ZG_MainChar"));
+	//DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+}
